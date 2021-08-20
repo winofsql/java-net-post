b04b34bea6905d90e0ed518af97420f3dd5fd102
sworc <lightbox@nifty.com>
2021-08-20 16:03:22 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,66 @@
+import java.net.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+
+            // URL文字列
+            String str = "https://lightbox.sakura.ne.jp/demo/json/post-test.php";
+
+            StringBuilder sb = new StringBuilder();
+            sb.append(str);
+            sb.append(String.format("?subject=%s", URLEncoder.encode("日本語表示", "utf-8") ));
+            sb.append(String.format("&name=%s", URLEncoder.encode("山田 太郎", "utf-8") ));
+            sb.append(String.format("&text=%s", URLEncoder.encode("こんにちは\r\nさようなら", "utf-8") ));
+            sb.append(String.format("&send=%s", URLEncoder.encode("送信", "utf-8") ));
+            sb.append(String.format("&datetime=%s", URLEncoder.encode("2020/11/04 11:51:25", "utf-8") ));
+
+            // ターゲット
+            URL url = new URL( sb.toString() );
+            // 接続オブジェクト
+            HttpURLConnection http = (HttpURLConnection)url.openConnection();
+            http.setConnectTimeout(30000);
+            http.setReadTimeout(30000);
+            http.setDoOutput(true);
+            http.setRequestMethod("POST");
+            // 接続 
+            http.connect();
+            
+            OutputStreamWriter osw = new OutputStreamWriter(http.getOutputStream());
+            BufferedWriter bw = new BufferedWriter(osw);
+
+            sb.setLength(0);
+            sb.append(String.format("subject=%s", URLEncoder.encode("件名", "utf-8") ));
+            sb.append(String.format("&name=%s", URLEncoder.encode("鈴木 次郎", "utf-8") ));
+            sb.append(String.format("&text=%s", URLEncoder.encode("ようこそ\r\nよろしくお願いいたします", "utf-8") ));
+            sb.append(String.format("&send=%s", URLEncoder.encode("送信ボタン", "utf-8") ));
+            sb.append(String.format("&datetime=%s", URLEncoder.encode("2020/11/04 00:00:00", "utf-8") ));
+
+            bw.write( sb.toString() );
+            bw.close();
+            osw.close();
+            
+            // UTF-8 でリーダーを作成
+            InputStreamReader isr = new InputStreamReader(http.getInputStream(), "utf-8");   
+            // 行単位で読み込む為の準備
+            BufferedReader br = new BufferedReader(isr);
+            String line_buffer;   
+            // BufferedReader は、readLine が null を返すと読み込み終了
+            while ( null != (line_buffer = br.readLine() ) ) {
+                System.out.println(line_buffer);
+            }
+
+            // 閉じる   
+            br.close();
+            isr.close();
+            http.disconnect();
+
+        }
+        catch( Exception e ) {
+            System.out.println( e.getMessage() );
+        }
+    }
+}
